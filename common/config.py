448b11b1f04bb2b25f4d861ef2b02b670b3edfc0
万明明 <wanmingming@wefresh.com>
2016-05-26 12:50:52 +0800

    version big change 2

--- common/config.py
+++ common/config.py
@@ -1,8 +1,8 @@
 #coding=utf-8
 __author__ = 'wan'
 
-
-
+import socket
+local_ip = socket.gethostbyname(socket.gethostname())
 
 ##########微信redis存储
 WX_KEY = "WX_CONFIG"
@@ -11,4 +11,7 @@ WX_ACCESS_TOKEN = "WX_ACCESS_TOKEN"
 
 
 ##########网站cookie保存域名#########
-DOMAIN = "localhost"
+if local_ip == "10.252.91.123":
+    DOMAIN = "http://wx.wefresh.me/"
+else:
+    DOMAIN = "localhost"
